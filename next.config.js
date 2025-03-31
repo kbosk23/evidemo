41593da88543d5fc40b644726fe7c8c9f6626ac8
Azure Deploy <deploy@azure.com>
2025-03-31 13:06:13 -0700

    Fix path aliases in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Add any valid Next.js config options here
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': '.',
+    };
+    return config;
+  },
 }
 
 module.exports = nextConfig 
